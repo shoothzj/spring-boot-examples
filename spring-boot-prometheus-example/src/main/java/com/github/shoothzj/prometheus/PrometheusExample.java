69983a9eca2b6dedbc825c756e4f090aa85ec15f
ZhangJian He <shoothzj@gmail.com>
2022-05-26 15:15:51 +0800

    complement pom file

---
+++ spring-boot-prometheus-example/src/main/java/com/github/shoothzj/prometheus/PrometheusExample.java
@@ -0,0 +1,13 @@
+package com.github.shoothzj.prometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusExample {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrometheusExample.class);
+    }
+
+}
