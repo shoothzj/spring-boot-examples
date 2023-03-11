43d3cf15189bf4602ee812ad710472360995737b
ZhangJian He <shoothzj@gmail.com>
2023-03-11 23:15:27 +0800

    add apollo jmx security example

---
+++ spring-boot-jmx-example/src/main/java/com/github/shoothzj/jmx/JmxExample.java
@@ -0,0 +1,11 @@
+package com.github.shoothzj.jmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmxExample {
+    public static void main(String[] args) {
+        SpringApplication.run(JmxExample.class);
+    }
+}
---
+++ spring-boot-security-example/src/main/java/com/github/shoothzj/security/SecurityExample.java
@@ -0,0 +1,11 @@
+package com.github.shoothzj.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityExample {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityExample.class);
+    }
+}
