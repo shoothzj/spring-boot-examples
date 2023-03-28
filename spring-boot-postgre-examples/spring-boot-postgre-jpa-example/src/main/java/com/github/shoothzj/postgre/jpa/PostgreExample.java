28bad8eeb02498c7b31d0c2fb6a357d6b010e48e
ZhangJian He <shoothzj@gmail.com>
2023-03-28 10:48:51 +0800

    add postgre example (#1)

---
+++ spring-boot-postgre-examples/spring-boot-postgre-jpa-example/src/main/java/com/github/shoothzj/postgre/jpa/PostgreExample.java
@@ -0,0 +1,11 @@
+package com.github.shoothzj.postgre.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreExample {
+    public static void main(String[] args) {
+        SpringApplication.run(PostgreExample.class);
+    }
+}
